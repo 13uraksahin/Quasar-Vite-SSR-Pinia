e9c0f7a1974f7c4d74836ec5ef47b3283d267739
13uraksahin <burak.sahin@pusula.com.tr>
2022-03-16 20:54:42 +0300

    changed to the script setup

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+
+  caption: {
+    type: String,
+    default: ''
+  },
+
+  link: {
+    type: String,
+    default: '#'
+  },
+
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
 <template>
   <q-item
     clickable
@@ -18,32 +44,3 @@
     </q-item-section>
   </q-item>
 </template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
-})
-</script>
